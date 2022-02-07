4371b8539afe1dfc5c68b4119ac83f3c2e529a86
ykim <ykim@ucomp.co.kr>
2022-02-08 03:49:30 +0900

    update

--- js/nav.js
+++ js/nav.js
@@ -10,17 +10,18 @@ $(document).ready(function () {
 	
   $('.header_area').mouseleave(function () {
     var scroll = $(window).scrollTop(); 
-		if( scroll >= 0 && scroll < 150 ) {
+		if( scroll < 150 ) {
 				$(this).css({'background':'rgba(255,255,255,0)','border-bottom':'none','box-shadow':'none'});
 				$('nav .nav-wrapper a').css('color','rgba(255,255,255,1)');
 				$('.header_area .header_logo a').css('background','url(../images/white_logo.png) center no-repeat');
-		}else if( scroll > 150 ){
+		} else {
 				$(this).css({'background':'rgba(255,255,255,1)','border-bottom':'1px solid #ccc','box-shadow':'0 0 10px #999'});
 				$('nav .nav-wrapper a').css('color','rgba(0,0,0,1)');
 				$('.header_area .header_logo a').css('background','url(../images/black_logo.png) center no-repeat');
 		}
 		on_off=false;    
-    });  
+    });
+  
 
     $(window).on('scroll',function () {
       var scroll = $(window).scrollTop();
@@ -32,7 +33,16 @@ $(document).ready(function () {
 					$('.header_area').css({'background':'rgba(255,255,255,0)','border-bottom':'none','box-shadow':'none'});
 					$('nav .nav-wrapper a').css('color','rgba(255,255,255,1)');
 					$('.header_area .header_logo a').css('background','url(../images/white_logo.png) center no-repeat');
-      };  
+      };
+		// $('.header_area').resize(function () {
+		// 	var inner_width = $(window).width();
+		// 	if ( inner_width < 768) {
+		// 			$('.header_logo a').css('background','url(../images/white_logo_m.png)no-repeat');
+		// 	} else if( inner_width > 768) {
+					
+		// 	} 
+		// }).resize(); 	
+	
   });
 
 });  
